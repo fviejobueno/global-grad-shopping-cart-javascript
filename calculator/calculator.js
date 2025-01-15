2aab697bd46ede2cfdcab419f3d724407be31069
einartech <imalovelyghost13@gmail.com>
2025-01-15 15:50:53 +0100

    einar branch

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,3 +1,15 @@
 export function add(a, b) {
-     return a + b
+  return a + b;
+}
+
+export function sustract(a, b) {
+  return a - b;
+}
+
+export function multiplicar(a, b) {
+  return a * b;
+}
+
+export function dividir(a, b) {
+  return a / b;
 }
--- calculator/calculator.test.js
+++ calculator/calculator.test.js
@@ -1,19 +1,50 @@
+import { add, sustract, multiplicar, dividir } from "./calculator";
+import { expect, test } from "vitest";
 
-import { add } from './calculator'
-import { expect, test } from 'vitest'
+test("Add numbers using the add method", () => {
+  //given
+  const x = 13;
+  const y = 5;
+  //when
+  const functionSuma = add(x, y);
+  //then
+  const resultadoEsperado = 18;
 
+  expect(functionSuma).toBe(resultadoEsperado);
+});
 
+test("Sustract numbers using the sustract method", () => {
+  //given
+  const x = 13;
+  const y = 3;
+  //when
+  const functionResta = sustract(x, y);
+  //then
+  const resultadoEsperado = 10;
 
-test('Add numbers using the add method', () => {
-     //given
-     const x = 13
-     const y = 0
-     //when
-     const functionSuma = add(x, y)
-     //then
-     const resultadoEsperado = 13
+  expect(functionResta).toBe(resultadoEsperado);
+});
 
-     expect(functionSuma).toBe(resultadoEsperado)
-     
+test("Multiplicar numbers using the multiplicar method", () => {
+  //given
+  const x = 10;
+  const y = 3;
+  //when
+  const functionMultiplicar = multiplicar(x, y);
+  //then
+  const resultadoEsperado = 30;
 
-})
+  expect(functionMultiplicar).toBe(resultadoEsperado);
+});
+
+test("Dividir numbers using the dividir method", () => {
+  //given
+  const x = 9;
+  const y = 3;
+  //when
+  const functionDividir = dividir(x, y);
+  //then
+  const resultadoEsperado = 3;
+
+  expect(functionDividir).toBe(resultadoEsperado);
+});
